b9576db77bf7f9d17b0507631ed8375016d89572
Sam.Chen <sam.chen2@luxshare-ict.com>
2025-09-24 15:33:25 -0700

    update session 3

---
+++ 3.lengthOfLongestSubstring/lengthOfLongestSubstring_set.py
@@ -0,0 +1,21 @@
+from collections import Counter
+
+
+class Solution:
+   def lengthOfLongestSubstring(self, s: str) -> int:
+        left = 0
+        seen = set()
+        ans = 0
+
+        for right in range(len(s)):
+            while s[right] in seen:
+                seen.remove(s[left])
+                left += 1
+            seen.add(s[right])
+            ans = max(ans, right - left + 1)
+
+        return ans
+
+sol = Solution()
+res = sol.lengthOfLongestSubstring('abcabcbb')
+print(res)
--- test.py
+++ test.py
@@ -1,5 +1,49 @@
 
+s = 'abccdef'
+left = 0
+right = 0
+cnt = [False for _ in range(26)]
+print(cnt)
+ans = 0
+while right < len(s):
+	while cnt[ord(s[right]) - ord('a')]:
+		cnt[ord(s[left]) - ord('a')] = False
+		left = left + 1
+	cnt[ord(s[right]) - ord('a')] = True 
 
-from typing import List, Optional
-import heapq
+	if right - left + 1 > ans:
+		ans = right - left + 1
+	print(f'right:{right} left:{left} ans:{ans}')
+	right = right + 1
+
+print(ans)
+print(len(s))
+'''
+right=0,s[right]='a'
+[1,0,0,0,0,0]
+ans=1
+right=1,s[right]='b'
+[1,1,0,0,0,0]
+ans=2
+right=2,s[right]='c'
+[1,1,1,0,0,0]
+ans=3
+right=3,s[right]='c',s[left]='a'
+[0,1,1,0,0,0]
+right=3,s[right]='c',s[left]='b'
+[0,0,1,0,0,0]
+right=3,s[right]='c',s[left]='c'
+[0,0,0,0,0,0]
+right=4,s[right]='d',
+[0,0,0,1,0,0]
+left=3,ans=3
+right=5,s[right]='e',
+[0,0,0,1,1,0]
+left=3,ans=3
+right=6,s[right]='f',
+[0,0,0,1,1,1]
+left=3,ans=3
+'''
+
+
 
