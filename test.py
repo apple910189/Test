e7f64cc7c6fd4f360dc6d8d8864b4b7d1019f816
Sam.Chen <sam.chen2@luxshare-ict.com>
2025-09-26 16:52:45 -0700

    sublime settins

--- test.py
+++ test.py
@@ -1,34 +1,32 @@
-class Solution:
-	def longestSubstringWithAtMostKDistinctCharacters(self, s: str, k:int) -> int:
-		left = 0
-		right = 0
-		# cnt = [0] * 128  # ASCII table size
-		ans = 0
-		cnt = {}
+import collections
 
-		while right < len(s):
-			if s[right] in cnt:
-				cnt[(s[right])] += 1
-			else:
-				cnt[(s[right])] = 1
-			while len(cnt) > k:
-				cnt[(s[left])] -= 1
-				if cnt[(s[left])] == 0:
-					del cnt[(s[left])]
-				left +=1
-			ans = max(ans, right - left + 1)
-			right += 1
-		return ans
+class Solution:
+    def longestSubstringWithAtMostKDistinctCharacters(self, s: str, k:int) -> int:
+        n = len(s)
+        left = 0
+        counter = collections.Counter()
+        ans = 0
+        for right in range(n):
+            counter[s[right]] +=1
+            while len(counter) > k:
+                counter[s[left]] -=1
+                if counter[s[left]] == 0:
+                    del counter[s[left]]
+                left +=1
+            ans = max(ans, right - left + 1)
+        return ans
+
 
 def printCnt(cnt):
-	print('[', end='')
-	for i,v in enumerate(cnt):
-		if v:
-			print(f'{i} ', end='')
-	print(']')
+    print('[', end='')
+    for i,v in enumerate(cnt):
+        if v:
+            print(f'{i} ', end='')
+    print(']')
 
 sol = Solution()
 ans = sol.longestSubstringWithAtMostKDistinctCharacters('abaccc',2)
 print(f'ans:{ans}')
 
 
+print(6.57/87.86)
