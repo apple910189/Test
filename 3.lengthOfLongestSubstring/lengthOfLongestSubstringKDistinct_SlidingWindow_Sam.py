d106d924ecb8fcbcddcc0b4873779465763c0517
Sam.Chen <sam.chen2@luxshare-ict.com>
2025-09-26 15:51:48 -0700

    s3: update comments

--- 3.lengthOfLongestSubstring/lengthOfLongestSubstringKDistinct_SlidingWindow_Sam.py
+++ 3.lengthOfLongestSubstring/lengthOfLongestSubstringKDistinct_SlidingWindow_Sam.py
@@ -31,4 +31,10 @@ sol = Solution()
 ans = sol.longestSubstringWithAtMostKDistinctCharacters('abaccc',2)
 print(f'ans:{ans}')
 
+'''
+利用right往右移動，每一次移動，都判斷當前對應的字符是否在字典裡，如果不在，就新增到字典，如果已經存在，就把對應的value加一。 
+在加入字典之前，要先判斷字典的長度，如果小於等於K，才能加入字典，如果大於K，要做兩件事。
+第一，字典中對應到當前left位置的字符value減一
+第二，left加一。 不斷重複這個動作，直到字典長度小於等於K
+'''
 
