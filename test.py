7353b6d8269d559c78839e1ef72c91d1cb83e72f
Sam.Chen <sam.chen2@luxshare-ict.com>
2025-10-02 13:15:49 -0700

    [S5] add 3sum

--- test.py
+++ test.py
@@ -2,7 +2,23 @@ from typing import List
 from collections import defaultdict
 
 class Solution:
-    def twoIntegerSumTarget(self, nums:List[int], target:int) -> List[List[int]]:
+    def twoSum(self, nums:List[int], target:int) -> List[List[int]]:
+        ans = []
+        hashmap = defaultdict(list)
+        for i, num in enumerate(nums):
+            diff = target - nums[i]
+            if hashmap[diff]:
+                j = hashmap[diff].pop()
+                ans = [j,i]
+            else:
+                hashmap[nums[i]].append(i)
+        return ans
+
+
+s = Solution()
+nums = [1, 2, 2,3,4,5]
+target = 9
+print(s.threeSum(nums, target))  # [[0, 3], [1, 2]]
 
 
 
