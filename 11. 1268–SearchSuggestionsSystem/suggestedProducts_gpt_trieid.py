831dada2652fde59a5b5e2c5121e5810217732d4
Sam.Chen <sam.chen2@luxshare-ict.com>
2025-10-17 14:40:51 -0700

    [S11] update gpt trieid

--- 11. 1268–SearchSuggestionsSystem/suggestedProducts_gpt_trieid.py
+++ 11. 1268–SearchSuggestionsSystem/suggestedProducts_gpt_trieid.py
@@ -1,7 +1,7 @@
 class TrieNode:
     def __init__(self):
         self.children = {}
-        self.words_idx = []  # 存索引，不存字串
+        self.words_idx = []  # 存索引（最多3個）
 
 class Trie:
     def __init__(self):
@@ -14,6 +14,7 @@ class Trie:
             if ch not in node.children:
                 node.children[ch] = TrieNode()
             node = node.children[ch]
+            # 插入索引，保持前三個最小字典序
             node.words_idx.append(word_idx)
             node.words_idx.sort()
             if len(node.words_idx) > 3:
@@ -27,7 +28,29 @@ class Trie:
                 node = node.children[ch]
                 result.append([products[i] for i in node.words_idx])
             else:
+                # prefix 不存在，之後都補空列表
                 while len(result) < len(prefix):
                     result.append([])
                 break
         return result
+
+
+class Solution:
+    def suggestedProducts(self, products, searchWord):
+        products.sort()  # 確保字典序
+        trie = Trie()
+
+        # 建立 Trie
+        for i in range(len(products)):
+            trie.insert(i, products)
+
+        # 搜尋每個 prefix
+        return trie.search(searchWord, products)
+
+
+sol = Solution()
+
+products = ["mobile","mouse","moneypot","monitor","mousepad"]
+searchWord = "mouse"
+ans = sol.suggestedProducts(products, searchWord)
+print(ans)
